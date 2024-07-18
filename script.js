7e26c1421779a306bfcb401dc30aacadc8ff706b
Shraddha0607 <136147634+Shraddha0607@users.noreply.github.com>
2024-07-18 11:32:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,13 +7,10 @@ function mincost(arr)
 	for(let i=0; i<arr.length-1; i++){
 		min_cost += arr[0]+arr[1];
 		arr[0] = arr[0]+ arr[1];
-        console.log("cost " + min_cost);
 		// now need to remove arr[i+1]
 		arr.splice(1,1);
-        console.log(arr);
 	}
     min_cost += arr[0]+arr[1];
-    console.log("final cost " + min_cost);
 	return min_cost;
   
 }
